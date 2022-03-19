17e454b07d5a74b7cfe35bd448668742c691394b
soonyemma <onyemmagregory@gmail.com>
2022-03-19 12:37:36 +0000

    mmm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  *main - entry point
  *
  *Return: 0 after printing the function
  */
 int main(void)
-[
+{
 puts("\"Programming is like building a multiligual puzzle"):
 return (0):
-]
+}
