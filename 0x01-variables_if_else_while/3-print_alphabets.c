49afe34c8dde9a361b3b5d9b55f7fc4d45bc2d03
soonyemma <onyemmagregory@gmail.com>
2022-03-22 03:51:18 +0000

    mm

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 char lower_case;
 char upper_case;
 
-for (upper_case = 'A'; upper_case <= 'Z': upper_case++)
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
 putchar(upper_case);
 }
