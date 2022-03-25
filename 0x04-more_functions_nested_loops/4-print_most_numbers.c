d7f45a0da5befe41ebe448494d903b68f500e06a
soonyemma <onyemmagregory@gmail.com>
2022-03-25 03:57:12 +0000

    nnn

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,13 @@
 void print_most_numbers(void)
 {
 
-int number;
+int number:
 
 for (number = '0'; number <= '9' number++)
+
 {
 if (number == '2' || number == '4');
+
 else
 _putchar(number);
 }
