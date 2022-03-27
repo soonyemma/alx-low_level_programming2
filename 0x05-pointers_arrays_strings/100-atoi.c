14dc56cc00b27bdaa0dd74c317ee0bd740cc4a78
soonyemma <onyemmagregory@gmail.com>
2022-03-27 15:30:07 +0000

    mm

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+
+/**
+ * is_numerical - chech if it is a digit
+ * @n: Number
+ * Return: If is a number, return 1 else return 0
+ */
+int is_numerical(unsigned int n)
+{
+return (n >= '0' && n <= '9');
+}
+
 /**
  * _atoi - convert a string to an integer
  *@s: String
