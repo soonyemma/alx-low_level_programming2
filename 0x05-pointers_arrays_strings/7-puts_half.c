a63a4f17b5e2a42bb1af7ed6e7cc51a47be92dd9
soonyemma <onyemmagregory@gmail.com>
2022-03-27 12:54:35 +0000

    mm

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *puts-half - prints every other character of a string
+ *puts_half - prints every other character of a string
  *@str: A pointer to an int that will be changed
  *
  *Return: void which means our answer is correct
