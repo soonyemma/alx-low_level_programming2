952d9ed25229acfdf8d7656f8168a7ddf874986f
soonyemma <onyemmagregory@gmail.com>
2022-03-28 12:16:54 +0000

    mm

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *s);
+char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
