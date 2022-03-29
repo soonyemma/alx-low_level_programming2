c3ec5b22c021bec441fc8ed12de15277a2bec5fc
soonyemma <onyemmagregory@gmail.com>
2022-03-29 02:00:46 +0000

    mm

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string in rot13;
- * @s: string to be encoded;
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
  *
- * Return: the resulting string;
+ * Return: the resulting string
  */
 char *rot13(char *s)
 {
