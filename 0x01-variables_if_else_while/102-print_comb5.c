79f973c61f17fb6008bbb778d73e506491ac9598
soonyemma <69128107+soonyemma@users.noreply.github.com>
2022-04-08 10:32:40 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,23 +8,23 @@ int main(void)
 {
 int i, j;
   
-  for (i = 0; i < 100; i++)
-  {
-    for (j = 0; j < 100; j++)
-    {
-      putchar((i / 10) + 48);
-      putchar((i % 10) + 48);
-      putchar('');
-      putchar((j / 10) + 48);
-      putchar((j % 10) + 48);
-      if (i != 98 || j != 99)
-      {
-        putchar(',');
-        putchar(' ');
-      }
-    }
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar('');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
     
-  }
+}
 }
 putchar('\n');
 return (0);
